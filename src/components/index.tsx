815a53211f293be011f9d14cb20ff81891ed57ab
Ash Entwisle <ash.entwisle@mailfence.com>
2023-03-20 18:13:15 +0000

    added base for index
    Signed-off-by: Ash Entwisle <ash.entwisle@mailfence.com>

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,14 +4,35 @@ export default function Index() {
     const title = "Hello World, I'm Ash!";
     return (
         <div class="index">
-            <h1 onMouseOver={(e) => animatedTyping(e)}>{ title }</h1>
-            <p>
-                Enjoyer of Arch, Lover of Rust, hater of JavaScript. 
-                Feel free to check out my <a href="/projects">projects</a> or my <a href="/blog">blog</a>. 
-            </p>
-            <p>
-                If you want to get in touch, <a href="/contact">feel free to reach out!</a>. 
-            </p>
+            <LandingBanner />
+            <LoLoMe />
+            <ContactBanner />
         </div>
     );
 }
+
+function LandingBanner() {
+    return (
+        <div class="landing-banner">
+            <h1>Whats Cooking?</h1>
+        </div>
+    );
+}
+
+function LoLoMe() {
+    return (
+        <div class="lolome">
+            // TODO: Add lolome
+        </div>
+    );
+}
+
+function ContactBanner() {
+    return (
+        <div class="contact-banner">
+            <h2>Get in Touch</h2>
+        </div>
+    );
+}
+
+// TODO: add stuff here
