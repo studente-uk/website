364c3c8bdffb956dac6f0905b96655e8ae557a62
Aren <20545498+arenmarsden@users.noreply.github.com>
2023-03-24 19:47:12 +0000

    Update header and footer (#12)
    Signed-off-by: Aren Marsden <aren.marsden6@gmail.com>

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,10 @@
-import motds from "../libs/motds";
+//import motds from "../libs/motds";
 
 export default function Footer() {
     return (
         <footer>
             <div class="logo"></div>
             <div class="footer">
-                <MOTD />
                 <Socials />
                 <Copyright />
             </div>
@@ -16,43 +15,32 @@ export default function Footer() {
     );
 }
 
-function MOTD() {
-    let motd = motds()
-    return (
-        <div class="motd">
-            <p><i>{ motd }</i></p>
-        </div>
-    );
-}
-
 function Socials() {
     return (
         <div class="socials">
             <div class="contact">
-            <p><a href="https://github.com/ash-entwisle">Github</a></p>
+            <p><a href="https://github.com/studente-uk">Github</a></p>
             </div>
             <div class="contact">
-            <p><a href="https://linkedin.com/in/ash-entwisle">LinkedIn</a></p>
+            <p><a href="https://linkedin.com/in/studente-uk">LinkedIn</a></p>
             </div>
             <div class="contact">
-            <p><a href="https://cohost.org/ash-e">Cohost</a></p>
+            <p><a href="https://twitter.com/studente-uk">Twitter</a></p>
             </div>
             <div class="contact">
-            <p><a href="https://hachyderm.io/@ashentwisle">Mastodon</a></p>
+            <p><a href="mailto://contact@studente.co.uk">Email</a></p>
             </div>
-            <div class="contact">
-            <p><a href="mailto://ash.entwisle@protonmail.com">Email</a></p>
-            </div>
+            
         </div>
     );
 }
 
 function Copyright() {
     let currentYear = new Date().getFullYear();
-    let output = currentYear > 2022 ? `2022-${currentYear}` : currentYear;
+    let output = currentYear > 2023 ? `2023-${currentYear}` : currentYear;
     return (
         <div class="copyright">
-            <p> &copy; <a href="https://ashentwisle.com/contact">Ash Entwisle</a> {output} </p>
+            <p> &copy; <a href="https://studente.co.uk/contact">Studente</a> {output} </p>
         </div>
     );
 }
