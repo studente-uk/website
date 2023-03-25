5f9fbdee4a660613c530406be463c2e759f01055
Aren Marsden <aren.marsden6@gmail.com>
2023-03-25 18:03:46 +0000

    start footer redesign
    Signed-off-by: Aren Marsden <aren.marsden6@gmail.com>

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -17,21 +17,25 @@ export default function Footer() {
 
 function Socials() {
     return (
-        <div class="socials">
-            <div class="contact">
-            <p><a href="https://github.com/studente-uk">Github</a></p>
+        <ul>
+            <div class="socials">
+                <li class="github">
+                    <p><img src="/assets/github.svg" class="icon"/><a href="https://github.com/studente-uk">Github</a></p>
+                </li>
+                <div class="contact">
+                   
+                </div>
+                <div class="contact">
+                    <p><a href="https://linkedin.com/in/studente-uk">LinkedIn</a></p>
+                </div>
+                <div class="contact">
+                    <p><a href="https://twitter.com/studente-uk">Twitter</a></p>
+                </div>
+                <div class="contact">
+                    <p><a href="mailto://contact@studente.co.uk">Email</a></p>
+                </div>
             </div>
-            <div class="contact">
-            <p><a href="https://linkedin.com/in/studente-uk">LinkedIn</a></p>
-            </div>
-            <div class="contact">
-            <p><a href="https://twitter.com/studente-uk">Twitter</a></p>
-            </div>
-            <div class="contact">
-            <p><a href="mailto://contact@studente.co.uk">Email</a></p>
-            </div>
-            
-        </div>
+        </ul>
     );
 }
 
