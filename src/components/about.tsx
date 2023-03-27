a28425d97b7f8bb9664a2448a11fbea957d44615
Owl Lord <114150402+JackHarvey186@users.noreply.github.com>
2023-03-27 16:08:33 +0100

    about us (#20)
    added fluff

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -7,6 +7,13 @@ export default function AboutUs() {
                 to help students develop their cooking skills through instructional videos and 
                 simple recipes.
             </p>
+            <p>
+                Studente came from a realisation that there were no real student focused recipe websites,
+                and we wanted to change that! We noticed that an alarming proportion of our peers were reliant
+                on heavily processed food and take-away meals because they may not know how to cook for themselves,
+                and we want to help students and other users to improve their cooking skills through our recipes,
+                and perhaps discover a love for cooking and preparing meals that was locked away.
+            </p>
         </div>
     )
 }
